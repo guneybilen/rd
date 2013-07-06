fb0909d6e81ab4d5b10117653f0d918514bee401
Guney Bilen <guneybilen@yahoo.com>
2013-07-07 01:33:32 +0300

    add named routes and improve rspec syntax

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,64 +1,68 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  subject { page } # now page.should can be replaced by should
+
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Referans Data')
-    end
+    before { visit root_path} # before is a synonym for before(:each){}
 
-    it "should have the h1 'Referans Data'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => "Referans Data") # :text need not be exact match, any substring would do as well
-    end
+    #it "should have the content 'Sample App'" do
+      #visit root_path
+      #page.should have_content('Referans Data')
+    #end
+    it { should have_content('ReferansData') }
 
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "Referans Data") # :text need not be exact match, any substring would do as well
-    end
+    #it "should have the h1 'Referans Data'" do
+    #  visit root_path
+    #  page.should have_selector('h1', :text => "Referans Data") # :text need not be exact match, any substring would do as well
+    #end
+    it { should have_selector('h1', text: 'ReferansData') }
 
-    it "should not have a custom page title" do
-      visit "/static_pages/home"
-      page.should_not have_selector("title", :text => " | Home")
-    end
+    #it "should have the base title" do
+      #visit root_path
+      #page.should have_selector('title', :text => "Referans Data") # :text need not be exact match, any substring would do as well
+    #end
+    it { should have_selector('title', text: full_title('') ) }
+
   end
 
   describe "Help page" do
 
-    it "should have the content 'help'" do
-      visit '/static_pages/help'
-      page.should have_content('Referans Data')
-    end
+    before { visit help_path }
 
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => "Referans Data") # :text need not be exact match, any substring would do as well
-    end
+    it { should have_content('ReferansData') }
 
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "Referans Data | Help") # :text need not be exact match, any substring would do as well
-    end
+    it { should have_selector('h1', :text => "ReferansData") } # :text need not be exact match, any substring would do as well
+
+
+    it { should have_selector('title', :text => full_title('Help') ) } # :text need not be exact match, any substring would do as well
 
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content("Referans Data")
-    end
+    before { visit about_path }
 
-    it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => "Referans Data") # :text need not be exact match, any substring would do as well
-    end
+    it { should have_content("ReferansData") }
 
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "Referans Data | About Us") # :text need not be exact match, any substring would do as well
-    end
+
+    it { should have_selector('h1', :text => "ReferansData") } # :text need not be exact match, any substring would do as well
+
+
+    it { should have_selector('title', :text => full_title('About Us') ) } # :text need not be exact match, any substring would do as well
+
+
+  end
+
+  describe "Contact page" do
+
+    before { visit contact_path }
+
+    it { should have_selector('h1', text: "Contact") }
+
+    it { should have_selector('title', text: full_title('Contact') ) } # :text need not be exact match, any substring would do as well
 
   end
 end
