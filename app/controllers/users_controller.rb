c8b00d83d99077ada7353eae9fd0ba001c7e038b
Guney Bilen <guneybilen@yahoo.com>
2013-08-02 22:16:32 +0300

    works on updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only:[:edit, :update]
+
+  def signed_in_user
+    redirect_to signin_path, notice: "Please sign in" unless signed_in?
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
 
   def new
     @user = User.new
