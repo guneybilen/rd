f7fa0e4332d18020586e1a1aaf94a2d49861fdef
Guney Bilen <guneybilen@yahoo.com>
2013-08-07 00:06:47 +0300

    works on updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only:[:edit, :update]
+  before_filter :signed_in_user, only:[:index, :edit, :update]
+  before_filter :correct_user, only:[:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user? @user
+  end
 
   def signed_in_user
-    redirect_to signin_path, notice: "Please sign in" unless signed_in?
+     unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in" unless signed_in?
+    end
   end
 
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
+    # correct_user before filter took care of: @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
@@ -17,7 +31,8 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
+    # correct_user before filter took care of: @user = User.find(params[:id])
   end
 
   def new
