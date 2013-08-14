beac2075f5a14ec034d102bfde2380d3586ec6fb
Guney Bilen <guneybilen@yahoo.com>
2013-08-14 22:32:33 +0300

    user post work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,13 @@ class PostsController < ApplicationController
   before_filter :signed_in_user, only: [:create, :destroy]
 
   def create
-
+    @post = current_user.posts.build(params[:post])
+    if @post.save
+      flash[:success] = "Post created!"
+      redirect_to root_path
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
