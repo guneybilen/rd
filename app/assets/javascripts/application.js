dad1450889fa29ab5804d33aadd7ab987b0009e9
Guney Bilen <guneybilen@yahoo.com>
2013-08-31 23:52:45 +0300

    commenting with js to position on screen using flesler/jquery.scrollTo plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,19 +15,29 @@
 //= require bootstrap
 //= require_tree .
 
-
 $( document ).ready(function() {
     $(".comment_form_reply_link").on('click', function(e){
-      var link = $(this).attr("id");
-        var text_box = $(this).next('.comment_form');
-      $(".comment_form:eq(" + link + ")").toggle().scrollTo(text_box);
-//        alert($(text_box).attr('id'));
-//      $('body');
-      $(".comment_form:gt(" + link + ")").css("display", "none");     // two forms of css parameters
-      $(".comment_form:lt(" + link + ")").css({"display" : "none"});  // taken from jQuery docs
 
-      e.preventDefault();
-    });
+        var link = $(this).attr("id");
+        var text_box = $(this).next('.comment_form');
+
+        $(".comment_form:eq(" + link + ")")
+            .animate({height: ["toggle", "swing"]}, "slow");
+
+        // .scrollTo() is a function of  flesler/jquery.scrollTo plugin
+        $('html').scrollTo( $(".comment_form:eq(" + link + ")"), 500, { offset:{ top:-150,left:0}});
+
+// things to keep in mind:
+// .animate({scrollTop: $(".comment_form:eq(" + link + ")").offset().top -100 }, 'slow');
+// if ( $(this).is(':visible'))
+// console.log('executed scrollToElement');
+
+
+        $(".comment_form:gt(" + link + ")").css("display", "none");     // two forms of css parameters
+        $(".comment_form:lt(" + link + ")").css({"display" : "none"});  // taken from jQuery docs
+
+              e.preventDefault();
+});
 
     $('.text_area_in_comment_form').html('');
 });
