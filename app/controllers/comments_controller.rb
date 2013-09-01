2c2d0169b0bf805ff9516ab7d0a2f2d91ca123bd
Guney Bilen <guneybilen@yahoo.com>
2013-09-01 20:17:56 +0300

    ajax semi working state

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,29 @@
 class CommentsController < ApplicationController
 
+  # NoMethodError (undefined method `stringify_keys' for "298":String):
+  # app/controllers/comments_controller.rb:9:in `create'
+  # hatasi aliyorsan parametreyi su sekilde
+  # gecir: .create(comment: params[:comment])
+
   def create
     if signed_in?
       @post = Post.find(params[:feed_item_id])
       @comment = @post.comments.create(comment: params[:comment])
+      @feed_items = current_user.feed.paginate(page: params[:page])
+      @hash = Hash.new
+      @feed_items.each do |feed_item|
+      feed_item.comments.each do |comment|
+          @hash["#{feed_item}"] = feed_item.comments.to_a
+        end
+      end
+
       flash[:success] = "Comment created!"
-      redirect_to root_path
+
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+
     else
       redirect_to signin_path
     end
