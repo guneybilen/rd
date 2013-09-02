602306089dcdf1e8e751b2f6865c26eab7588a24
Guney Bilen <guneybilen@yahoo.com>
2013-09-02 23:48:10 +0300

    ajax commenting big step accomplish

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,14 +8,23 @@ class CommentsController < ApplicationController
   def create
     if signed_in?
       @post = Post.find(params[:feed_item_id])
+      @i_is_for_comments_partial = params[:i]
       @comment = @post.comments.create(comment: params[:comment])
+      @feed_item = @post
+      puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #{@post.id}"
       @feed_items = current_user.feed.paginate(page: params[:page])
+      #@feed_items = @feed_items.reload
       @hash = Hash.new
       @feed_items.each do |feed_item|
-      feed_item.comments.each do |comment|
+        #puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ #{@hash[293].nil?}"
+        #if feed_item.id.to_s == '293'
+        feed_item.comments.each do |comment|
+          puts feed_item
           @hash["#{feed_item}"] = feed_item.comments.to_a
         end
+          #end
       end
+      puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ #{@feed_item.id}"
 
       flash[:success] = "Comment created!"
 
