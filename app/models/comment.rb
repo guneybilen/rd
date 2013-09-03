4ef01673285ffcd159209cdb02cf8edcd29bb7c2
Guney Bilen <guneybilen@yahoo.com>
2013-09-03 18:45:28 +0300

    unstable state

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   attr_accessible :comment, :post_id, :failure, :success
 
   belongs_to :post
+  belongs_to :user
 
   validates :post_id, :presence => true
   validates :comment, :presence => true
--- db/migrate/20130829144627_create_comments.rb
+++ db/migrate/20130829144627_create_comments.rb
@@ -2,6 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.integer :post_id, :null => false
+      t.integer :user_id, :null => false
       t.text :comment, :limit => 3000
       t.integer :success, :default => 0
       t.integer :failure, :default => 0
