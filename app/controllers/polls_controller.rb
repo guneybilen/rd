0183b7858fc502e1489f25d303834a2cd53025da
Guney Bilen <guneybilen@yahoo.com>
2013-09-16 21:16:55 +0300

    posts count on the view placed

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -5,6 +5,8 @@ def index
   @poll_posts = Post.where("created_at > ?", Time.at(params[:after].to_i + 50))
                     .paginate(page: params[:page])
 
+  @poll_posts_count = @poll_posts.count
+
    unless current_user.nil?
     #puts "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{@poll_posts.last.user.name}"
     @result = current_user.posts.include?(Post.first)
